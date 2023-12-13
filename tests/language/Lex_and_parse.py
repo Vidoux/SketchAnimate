311d9fb57febad5904161a050bd871d9a8c55f26
Baptiste LE ROUX <bapt.leroux29@gmail.com>
2023-12-13 19:49:03 +0100

    lexer parser : import libs

--- tests/language/Lex_and_parse.py
+++ tests/language/Lex_and_parse.py
@@ -1,7 +1,7 @@
 import sys
 from antlr4 import FileStream, CommonTokenStream
 
-import tests.language.ErrorListenerTest
+from tests.language.ErrorListenerTest import ErrorListenerTest
 from src.language.codegen.antlr_build.SketchAnimateImperativeParadigmLexer import SketchAnimateImperativeParadigmLexer
 from src.language.codegen.antlr_build.SketchAnimateImperativeParadigmParser import SketchAnimateImperativeParadigmParser
 from SketchAnimateExecutor import SketchAnimateExecutor
@@ -11,7 +11,7 @@ input_stream = FileStream("example2_Imperative.ska")
 lexer = SketchAnimateImperativeParadigmLexer(input_stream)
 token_stream = CommonTokenStream(lexer)
 parser = SketchAnimateImperativeParadigmParser(token_stream)
-parser.addErrorListener(tests.language.ErrorListenerTest.ErrorListenerTest())
+parser.addErrorListener(ErrorListenerTest())
 tree = parser.program()  # 'program' is root rule
 
 executor = SketchAnimateExecutor()
