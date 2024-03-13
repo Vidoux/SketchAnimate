b307689c6065743bf1cd95a412000878621d55dc
Baptiste LE ROUX <bapt.leroux29@gmail.com>
2024-03-13 21:47:11 +0100

    update lex_and_parse test : add utf-8 prot

--- tests/language/Lex_and_parse.py
+++ tests/language/Lex_and_parse.py
@@ -3,10 +3,10 @@ from antlr4 import FileStream, CommonTokenStream
 from tests.language.ErrorListenerTest import ErrorListenerTest
 from src.language.codegen.antlr_build.SketchAnimateImperativeParadigmLexer import SketchAnimateImperativeParadigmLexer
 from src.language.codegen.antlr_build.SketchAnimateImperativeParadigmParser import SketchAnimateImperativeParadigmParser
-from tests.language.SketchAnimateExecutor import SketchAnimateExecutor
+from src.language.executor.SketchAnimateExecutor import SketchAnimateExecutor
 
 # Create Lexer and Parser
-input_stream = FileStream("JO_script.ska")
+input_stream = FileStream("JO_script.ska", encoding='utf-8')
 lexer = SketchAnimateImperativeParadigmLexer(input_stream)
 token_stream = CommonTokenStream(lexer)
 print(token_stream.getText())
